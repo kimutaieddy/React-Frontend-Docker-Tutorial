9ef9ce51db69c58b446b595909f741708aa15c6f
kimutaieddy <eddykimutai@outlook.com>
2024-05-30 22:49:33 +0300

    redering time on the app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const TimeDisplay = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000); // Update time every second
+
+    return () => clearInterval(intervalId); // Cleanup on unmount
+  }, []);
+
+  return (
+    <div className="time-display">
+      Current Time: {time.toLocaleTimeString()}
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <TimeDisplay /> {/* Add the TimeDisplay component here */}
   </React.StrictMode>
 );
 
