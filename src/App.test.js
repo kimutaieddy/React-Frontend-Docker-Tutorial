59a50048d3f930c04c95cca322c9e2958dd7c0d5
kimutaieddy <eddykimutai@outlook.com>
2024-06-16 21:25:19 +0300

    Adjust text matcher to match an existing element

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/click here for a surprise/i); // Adjust text matcher to match an existing element
   expect(linkElement).toBeInTheDocument();
 });
